67a31d326164a23bf5132a82b2de9ef5c87fc6fc
chandra <chandra@CHANDU>
2016-07-09 15:57:10 -0400

    Add project files.

---
+++ COMP2007-Lab4-S2016/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_Lab4_S2016
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ COMP2007-Lab4-S2016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2007_Lab4_S2016.MvcApplication" Language="C#" %>
